aeceb759f47024c5b7b98d8069aaf23e5b15922e
Ivan <ilomeli325@gmail.com>
2018-04-11 21:27:08 -1000

    workout
    listing workouts

---
+++ app/imports/ui/components/Workout.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Card, Image } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+class Workout extends React.Component {
+  render() {
+    return (
+        <Card centered>
+          <Card.Content>
+            <Image floated='right' size='mini' src={this.props.workout.image}/>
+            <Card.Header>
+              {this.props.workout.name}
+            </Card.Header>
+            <Card.Meta>
+            {this.props.workout.category}
+          </Card.Meta>
+            <Card.Description>
+              {this.props.description}
+            </Card.Description>
+          </Card.Content>
+        </Card>
+    );
+  }
+}
+
+Workout.propTypes = {
+  workout: PropTypes.object.isRequired,
+};
+
+export default withRouter(Workout);

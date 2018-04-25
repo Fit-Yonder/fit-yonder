821ba4289387f4ab1d01e1be7fc1d36874999e4c
rknakata <rknakata@hawaii.edu>
2018-04-24 21:55:13 -1000

    fix landing page logo

--- app/imports/ui/components/LandingWelcome.jsx
+++ app/imports/ui/components/LandingWelcome.jsx
@@ -9,7 +9,7 @@ class LandingWelcome extends React.Component {
 
     };
     const landingWelcomeText = {
-      paddingTop: '15em',
+      paddingTop: '5em',
     };
     const whiteText = {
       color: 'white',
@@ -31,7 +31,7 @@ class LandingWelcome extends React.Component {
             <h1 style={clearBackground} className='border-outline'>Welcome to a new way to exercise</h1>
             <br></br>
 
-            <img src='https://github.com/Fit-Yonder/Fit-Yonder.github.io/blob/master/images/FitYonderLogo.png?raw=true'/>
+            <img src='https://github.com/Fit-Yonder/Fit-Yonder.github.io/blob/master/images/FitYonderLogo.png?raw=true' height="200" width="200"/>
             <h4 style={whiteText} className='border-outline'>Fit Yonder is an application providing a platform for students and health and fitness
               professionals that allows them to post ideas for flexible and personalized fitness routines, things
               such as yoga etc. that can be done in one’s dorm room, or somewhere on campus.</h4>

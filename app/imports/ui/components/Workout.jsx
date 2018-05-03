408a80f8193160e0dfcc487fb89881119b7c366a
rknakata <rknakata@hawaii.edu>
2018-05-02 17:52:40 -1000

    checkpoint

--- app/imports/ui/components/Workout.jsx
+++ app/imports/ui/components/Workout.jsx
@@ -1,48 +1,80 @@
 import React from 'react';
-import { Card, Button, Image, Segment } from 'semantic-ui-react';
+import { Grid, Loader, Header, Segment } from 'semantic-ui-react';
+import { Profiles, ProfileSchema } from '/imports/api/profile/profile';
+import { Bert } from 'meteor/themeteorchef:bert';
+import AutoForm from 'uniforms-semantic/AutoForm';
+import TextField from 'uniforms-semantic/TextField';
+import LongTextField from 'uniforms-semantic/LongTextField';
+import SubmitField from 'uniforms-semantic/SubmitField';
+import HiddenField from 'uniforms-semantic/HiddenField';
+import ErrorsField from 'uniforms-semantic/ErrorsField';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import { withRouter, Link } from 'react-router-dom';
 
-class Workout extends React.Component {
+/** Renders the Page for editing a single document. */
+class EditProfile extends React.Component {
 
+  /** On successful submit, insert the data. */
+  submit(data) {
+    const { firstName, lastName, description, image, _id } = data;
+    // const workouts = Profiles.findOne({ owner: 'admin@foo.com' });
+    // const workoutsCount = ;
+    // const { firstName, lastName, address, image, description, _id } = data;
+    // schema might cause error because its missing workotus workouts count in update call
+    Profiles.update(_id, { $set: { firstName, lastName, description, image } }, (error) => (error ?
+        Bert.alert({ type: 'danger', message: `Update failed: ${error.message}` }) :
+        Bert.alert({ type: 'success', message: 'Update succeeded' })));
+  }
+
+  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
-    const cardstyle = {
-      color: 'white',
-      padding: '2em',
-    };
+    return (this.props.ready) ? this.renderPage() : <Loader>Getting data</Loader>;
+  }
 
-
+  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+  renderPage() {
     return (
-        <Card fluid color='blue'>
-          <div className='blue-background' style={cardstyle}>
-            <Card.Content>
-              <Image floated='right' height="250" width="250" src={this.props.workout.image}/>
-              <Card.Header>
-                {this.props.workout.name}
-              </Card.Header>
-              <Card.Meta color='white'>
-                {this.props.workout.category}
-              </Card.Meta>
-              <Card.Description>
-                {this.props.workout.description}
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <div className='blue-background'>
-                <Button fluid circular color='green'> Join workout! </Button>
-              </div>
-            </Card.Content>
-            <Card.Content extra>
-              <Link to={`/edit/${this.props.workout._id}`}>Edit</Link>
-            </Card.Content>
-          </div>
-        </Card>
+        <Grid container centered>
+          <Grid.Column>
+            <Header as="h2" inverted textAlign="center">Edit Profile</Header>
+            <AutoForm schema={ProfileSchema} onSubmit={this.submit} model={this.props.doc}>
+              <Segment>
+                <TextField name='firstName'/>
+                <TextField name='lastName'/>
+                <LongTextField name='description'/>
+                <TextField name='image'/>
+                <SubmitField value='Submit'/>
+                <ErrorsField/>
+                <HiddenField name='owner'/>
+                <HiddenField name='workouts'/>
+                <HiddenField name='workoutsCount'/>
+              </Segment>
+            </AutoForm>
+          </Grid.Column>
+        </Grid>
     );
   }
 }
 
-Workout.propTypes = {
-  workout: PropTypes.object.isRequired,
+/** Require the presence of a Profile document in the props object. Uniforms adds 'model' to the props, which we use. */
+EditProfile.propTypes = {
+  doc: PropTypes.object,
+  model: PropTypes.object,
+  ready: PropTypes.bool.isRequired,
 };
 
-export default withRouter(Workout);
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(({ match }) => {
+  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
+  const documentId = match.params._id;
+  // Get access to Profiless documents.
+  const subscription = Meteor.subscribe('Profiles');
+
+
+  return {
+    doc: Profiles.findOne({ owner: Meteor.user().username }),
+    ready: subscription.ready(),
+  };
+})(EditProfile);
+
